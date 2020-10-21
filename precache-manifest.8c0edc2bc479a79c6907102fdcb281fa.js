c1ca2216a005492c1fb72c86d55d8c4b54773f05
Woonhian <limwoonhian@hotmail.com>
2020-10-21 22:36:00 +0800

    Updates

---
+++ precache-manifest.8c0edc2bc479a79c6907102fdcb281fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2d78b8bfedbe67177e955a27f41442",
+    "url": "/Star-Wars/index.html"
+  },
+  {
+    "revision": "968f8b0eefe5095110fb",
+    "url": "/Star-Wars/static/js/2.c2cdcb46.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Star-Wars/static/js/2.c2cdcb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953d01bbd4a4f7ea8c8a",
+    "url": "/Star-Wars/static/js/main.0e9dd7ff.chunk.js"
+  },
+  {
+    "revision": "11783fd4f2f19e34e9ad",
+    "url": "/Star-Wars/static/js/runtime-main.892f4b3b.js"
+  }
+]);
